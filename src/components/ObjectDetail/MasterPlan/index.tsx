b444b6541dc2b5aaa5f63db59f5cd97434c53ea2
buranJ <zhunusburan@gmail.com>
2025-03-26 06:58:32 -0700

    location | nums

--- src/components/ObjectDetail/MasterPlan/index.tsx
+++ src/components/ObjectDetail/MasterPlan/index.tsx
@@ -2,12 +2,32 @@ import { FC } from 'react';
 
 import { INumeration } from 'types/common/objects';
 
+import bg from "../../../assets/images/nums.jpg"
+
 import './style.scss'
 
 const MasterPlan: FC<INumeration> = ({ gen_plan, image }) => {
   return (
-    <></>
+    <>
+      <section className="number">
+        <div className="container">
+          <div className="number__content">
+            <div className="number__title">
+              <h2 className="font-bold text-[22px] text-center md:text-[50px]">Нумерация и Расположение</h2>
+            </div>
+            <div className="number__img relative">
+              <img className="mt-[30px] m-auto" src={bg} alt="body-img" />
+              <a className="absolute bottom-[3%] right-[5%] md:right-[10%] text-[14px] border border-white rounded-[62px] px-[10px] py-[10px] md:py-[15px] md:px-[25px] md:text-[25px] backdrop-blur-[10px] bg-[rgba(0,9,27,0.1)] font-medium leading-[110%] text-center text-white" download href="#">
+                Скачать генплан</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
 export default MasterPlan;
+
+
+
--- src/components/ObjectDetail/Panarama.tsx
+++ src/components/ObjectDetail/Panarama.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 import Hero from 'components/Hero'
 
 import AboutObject from './AboutObject'
+import Location from './Location'
+import MasterPlan from './MasterPlan'
 
 function Panarama() {
     return (
         <>
             <Hero />
             <AboutObject />
+            <MasterPlan />
+            <Location />
         </>
     )
 }
