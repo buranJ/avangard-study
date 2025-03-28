f1714cbd772b9ba0f123df41772ea879522228d4
buranJ <zhunusburan@gmail.com>
2025-03-28 06:10:27 -0700

    beforafter

--- src/components/ObjectDetail/BeforeAfter/index.tsx
+++ src/components/ObjectDetail/BeforeAfter/index.tsx
@@ -1,20 +1,40 @@
-import { FC } from 'react';
-import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
+import { FC } from "react";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
 
-import img from 'assets/icons/handleRange.svg';
+import day from "../../../assets/images/BeforeAfter/before.webp"
 
-import './style.scss';
+import night from "../../../assets/images/BeforeAfter/after.jpg"
+import img from "assets/icons/handleRange.svg";
+
+import "./style.scss";
 
 interface IProps {
   building: number;
-  day_image: string
+  day_image: string;
   id: number;
-  night_image: string
+  night_image: string;
 }
 
 const BeforeAfter: FC<IProps> = ({ day_image, night_image }) => {
   return (
     <>
+      <div className="beforeAfter">
+        <div className="container">
+          <div className="beforeAfter__content">
+            <h2 className="beforeAfter__title">Атмосфера вечернего парка</h2>
+            <div className="beforeAfter__slider">
+              <ReactCompareSlider
+                itemOne={<ReactCompareSliderImage src={day} alt={day_image} className="beforeAfter__slider-image" />}
+                itemTwo={<ReactCompareSliderImage src={night} alt={night_image} className="beforeAfter__slider-image" />}
+                handle={<img className="beforeAfter__handle" src={img} alt="handle" />}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/ObjectDetail/Panarama.tsx
+++ src/components/ObjectDetail/Panarama.tsx
@@ -5,12 +5,14 @@ import Hero from 'components/Hero'
 import AboutObject from './AboutObject'
 import Location from './Location'
 import MasterPlan from './MasterPlan'
+import BeforeAfter from './BeforeAfter'
 
 function Panarama() {
     return (
         <>
             <Hero />
             <AboutObject />
+            <BeforeAfter />
             <MasterPlan />
             <Location />
         </>
