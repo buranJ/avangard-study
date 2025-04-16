a05f825c9a56ae9aa93a1320cfd266d0e864d650
buranJ <zhunusburan@gmail.com>
2025-04-16 07:59:57 -0700

    hero

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,6 +1,8 @@
 import { FC, useEffect, useState } from 'react';
 import './styles.scss';
 
+import { useGetHomeInfoQuery } from 'api/home/home.api';
+
 interface CompanyStats {
   since: number;
   trust: string;
@@ -25,13 +27,17 @@ export const companyData: CompanyStats = {
   operationalStatus: "Введено в эксплуатацию",
 };
 
+
 const Hero: FC = () => {
+  const { data, } = useGetHomeInfoQuery();
+ 
   const [animatedSince, setAnimatedSince] = useState(0);
   const [animatedProjectsBuilt, setAnimatedProjectsBuilt] = useState(0);
   const [animatedTotalArea, setAnimatedTotalArea] = useState(0);
 
   useEffect(() => {
-    const animateNumber = (start: number, end: number, duration: number, callback: (value: number) => void) => {
+    if (!data) return;
+    function animateNumber(start: number, end: number, duration: number, callback: (value: number) => void) {
       let startTime: number | null = null;
 
       const updateNumber = (currentTime: number) => {
@@ -46,23 +52,31 @@ const Hero: FC = () => {
       };
 
       requestAnimationFrame(updateNumber);
-    };
+    }
 
     animateNumber(0, companyData.since, 2000, setAnimatedSince);
     animateNumber(0, companyData.projectsBuilt, 2000, setAnimatedProjectsBuilt);
     animateNumber(0, companyData.totalArea, 2000, setAnimatedTotalArea);
   }, []);
-
+  if (!data) return null;
   return (
     <div className="hero">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src={data.banner}
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
       <div className="container">
         <div className="hero__content md:flex justify-around items-center">
           <div className="hero__text">
             <div className="hero__text-big">
-              <h2 className="hero__since md:text-[80px]">
+              <h2 className="hero__since md:text-[80px] font-normal text-[80px] leading-[110%] text-center">
                 SINCE {animatedSince}
               </h2>
-              <p className="hero__description md:text-[25px]">
+              <p className="hero__description font-medium text-[25px] leading-[110%] text-center">
                 {companyData.trust}
               </p>
             </div>
@@ -70,18 +84,18 @@ const Hero: FC = () => {
           <div className="hero__buttom">
             <div className="hero__buttom-small">
               <h2 className="hero__buttom-title md:text-[80px]">
-                {`>${animatedTotalArea.toLocaleString()} м²`}
-              </h2>
-              <p className="hero__buttom-description md:text-[25px]">
-                {companyData.operationalStatus}
-              </p>
-            </div>
-            <div className="hero__buttom-small">
-              <h2 className="hero__buttom-title md:text-[80px]">
                 &gt;{animatedProjectsBuilt}
               </h2>
               <p className="hero__buttom-description md:text-[25px]">
                 Построили объектов
+              </p>
+            </div>
+            <div className="hero__buttom-small">
+              <h2 className="hero__buttom-title md:text-[80px]">
+                {`>${animatedTotalArea.toLocaleString()} м²`}
+              </h2>
+              <p className="hero__buttom-description md:text-[25px]">
+                {companyData.operationalStatus}
               </p>
             </div>
           </div>
